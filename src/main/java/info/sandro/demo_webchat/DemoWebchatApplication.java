3165958176097e009b783143984091c65cd078f2
sandroalmeida <sandro.almeida@dotbuild.com.br>
2019-09-20 08:08:18 -0700

    First commit

---
+++ src/main/java/info/sandro/demo_webchat/DemoWebchatApplication.java
@@ -0,0 +1,13 @@
+package info.sandro.demo_webchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebchatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/info/sandro/demo_webchat/DemoWebchatApplicationTests.java
@@ -0,0 +1,16 @@
+package info.sandro.demo_webchat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoWebchatApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
